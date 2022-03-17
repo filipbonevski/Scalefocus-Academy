7373dbb3e105cb5dc5e91d12926d950776ce77dc
Filip Bonevski <f_bonevski@hotmail.com>
2022-03-17 18:03:45 +0100

    Added homework from week4/lecture11

--- week4/lecture11/main.go
+++ week4/lecture11/main.go
@@ -2,75 +2,61 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"math/rand"
 	"sync"
-	"time"
 )
 
 func processEven(inputs []int) chan int {
 	intChan := make(chan int)
 	var wg sync.WaitGroup
+	go func() {
+		for _, i := range inputs {
+			wg.Add(1)
 
-	for _, i := range inputs {
-		wg.Add(1)
+			go func(i int) {
+				defer wg.Done()
+				if i%2 == 0 {
+					intChan <- i
+				}
+			}(i)
+		}
+		wg.Wait()
+		close(intChan)
+	}()
 
-		go func(i int) {
-			defer wg.Done()
-			time.Sleep(time.Duration(rand.Intn(2000)) * time.Millisecond)
-
-			intChan <- i
-			if i%2 == 0 {
-				log.Println("input: ", i)
-				// intChan <- i
-			}
-
-		}(i)
-		<-intChan
-	}
-
-	close(intChan)
-	wg.Wait()
 	return intChan
 }
 
 func processOdd(inputs []int) chan int {
 	intChan := make(chan int)
 	var wg sync.WaitGroup
+	go func() {
+		for _, i := range inputs {
+			wg.Add(1)
 
-	for _, i := range inputs {
-		wg.Add(1)
+			go func(i int) {
+				defer wg.Done()
+				if i%2 == 1 {
+					intChan <- i
+				}
+			}(i)
+		}
+		wg.Wait()
+		close(intChan)
+	}()
 
-		go func(i int) {
-			defer wg.Done()
-			time.Sleep(time.Duration(rand.Intn(2000)) * time.Millisecond)
-
-			intChan <- i
-			if i%2 == 1 {
-				log.Println("input: ", i)
-				// intChan <- i
-			}
-
-		}(i)
-		<-intChan
-	}
-
-	close(intChan)
-	wg.Wait()
 	return intChan
-
 }
 
 func main() {
-	inputs := []int{1, 17, 34, 56, 2, 8}
+	inputs := []int{1, 17, 33, 56, 2, 9}
 	chEven := processEven(inputs)
 	chOdd := processOdd(inputs)
 
 	for even := range chEven {
-		fmt.Println(even)
+		fmt.Println("Even:", even)
 	}
 
 	for odd := range chOdd {
-		fmt.Println(odd)
+		fmt.Println("Odd:", odd)
 	}
 }
